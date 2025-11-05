adf384d5297364720576c816b607671bb63d87b9
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-11-05 06:32:00 -0300

    add iterative fibs function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,52 @@ import './styles.css';
 
 console.log('AYO AYO!');
 
-function fibs() {
+function fibonacci(num) {
 
-    console.log('friends know me as fibs');
+    let xMinus2 = 0;
+    let xMinus1 = 1;
+    let x = 0;
+
+    if (num < 0) {
+
+        return 'OOPS';
+
+    } else {
+
+    for (let i = 1; i <= num; i++) {
+
+        xMinus2 = xMinus1
+        
+        xMinus1 = x
+
+        x = xMinus2 + xMinus1
+
+    };
+
+    return x;
+
+    };
 
 };
 
-fibs();
+function fibs(num) {
+
+    let fibArray = [];
+
+    for (let i = 0; i < num; i++) {
+
+        fibArray.push(fibonacci(i));
+
+    }
+
+    return fibArray;
+
+}
+
+console.log(fibs(8));
+
+function fibsRec(num) {
+
+    
+}
+
