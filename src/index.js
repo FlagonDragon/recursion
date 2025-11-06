7e37b0c24aad9fa61d8503b1f96ce01fbdd1e92a
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-11-06 04:52:02 -0300

    add fibsRec function

--- src/index.js
+++ src/index.js
@@ -29,11 +29,83 @@ function fibs(num) {
 
 };
 
-console.log(fibs(3));
+function fibsRec2(num) {
+
+    let fibsArray = [];
+
+    if (num < 1) {
+        return fibsArray;
+    } 
+
+    if (num == 1) {
+        return fibsArray = [0];
+    } 
+
+    if (num == 2) {
+        return fibsArray = [1];
+    }
+
+    return fibsArray = [fibsRec2(num - 1)[0] + fibsRec2(num - 2)[0]];
+    
+};
+
+function fibsRec3(num) {
+
+    console.log("This was printed recursively");
+
+    if (num < 1) {
+        return [];
+    } 
+
+    if (num == 1) {
+        return [0];
+    } 
+
+    return fibsRec3(num-1).concat(fibsRec2(num));
+    
+};
 
 function fibsRec(num) {
 
-    console.log(num);
+    let fibsArray = [];
+
+    if (num < 1) {
+        return fibsArray;
+    } 
+
+    if (num == 1) {
+        return fibsArray = [0];
+    } 
+
+    if (num == 2) {
+        return fibsArray = [0,1];
+    }
+
+    fibsArray = [fibsRec(num - 1)[fibsRec(num - 1).length - 1] + fibsRec(num - 2)[fibsRec(num - 2).length - 1]];    
+
+    return fibsRec(num-1).concat(fibsArray);
     
-}
+};
+
+console.log(fibs(8));
+
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(2));
+console.log(fibsRec(3));
+console.log(fibsRec(4));
+console.log(fibsRec(5));
+console.log(fibsRec(6));
+console.log(fibsRec(7));
+console.log(fibsRec(8));
+console.log(fibsRec(9));
+console.log(fibsRec(10));
+console.log(fibsRec(11));
+console.log(fibsRec(12));
+console.log(fibsRec(13));
+console.log(fibsRec(14));
+console.log(fibsRec(15));
+
+
+
 
