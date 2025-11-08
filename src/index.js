051cf7c651fcb38d14e60926263382437620cb66
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-11-08 05:11:25 -0300

    add mergeSort function

--- src/index.js
+++ src/index.js
@@ -91,6 +91,12 @@ function merge3(A, B) {
 
 function merge(A, B) {
 
+    console.log('Merging '+A+' and '+B);
+
+    if (!B) {
+        return A;
+    }
+    
     let C = []
     let i = 0;
     let j = 0;
@@ -100,13 +106,13 @@ function merge(A, B) {
 
         if (A[i] < B[j]) {
 
-            console.log(A[i]+'<'+B[j]);
+            // console.log(A[i]+'<'+B[j]);
 
             C[k++] = A[i++];
 
         } else {
 
-            console.log(B[j]+'<'+A[i]);
+            // console.log(B[j]+'<'+A[i]);
           
             C[k++] = B[j++];
   
@@ -126,17 +132,25 @@ function merge(A, B) {
 
     }
 
+    console.log('Merged result: '+C);
     return C;
 
 }
 
-console.log(merge(myArrayA, myArrayB));
+// console.log(merge(myArrayA, myArrayB));
+// console.log(merge([8]));
+// console.log(merge([8], [2]));
 
-let myArray = [10, 5];
+
+let shortArray = [10, 5];
 
 function sort(array) {
 
     let newArray = []
+
+    if (array.length == 1) {
+        return array;
+    }
 
     array[0] > array[1] ? newArray.push(array[1], array[0]) : newArray.push(array[0], array[1]);
 
@@ -144,15 +158,50 @@ function sort(array) {
 
 };
 
-console.log(sort(myArray));
+// console.log(sort(shortArray));
+
+let myArray = [6, 2, 5, 3, 8, 4, 1, 9];
+// let myArray = [6, 2, 5, 3];
+// let myArray = [6, 2];
+
+
 
 
 function mergeSort(array) {
 
-    return array;
+    let start = array.indexOf(array[0]);
+    let end = array.length - 1;
+
+    console.log('array: '+array);
+
+    if (start < end)  {
+
+        let mid = Math.floor((start+end)/2);
+        let firstHalf = array.slice(start, mid+1);
+        let secondHalf = array.slice(mid+1, end+1);
+
+        console.log('firstHalf: '+firstHalf)
+        console.log('secondHalf: '+secondHalf);
+
+        firstHalf = mergeSort(firstHalf);
+        secondHalf = mergeSort(secondHalf);
+
+        console.log('New firstHalf: '+firstHalf)
+        console.log('New secondHalf: '+secondHalf);
+
+        return merge(firstHalf, secondHalf);
+
+    } else {
+        return array;
+    }
 
 };
 
-// console.log(mergeSort(myArray));
+console.log(mergeSort(myArray));
+// console.log(merge([5],[3]));
+// console.log(merge([6],[2]));
+// console.log(merge(merge([6],[2]),merge([5],[3])));
+
+
 
 
